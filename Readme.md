237d351fd5828203ad5f520f2f5d13d7c9ff92da
Muhammad Usman <usmanlive399@gmail.com>
2024-09-01 12:32:05 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Q-13 of 45 days codding challenge
+# q-13 ts/js
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let transporation = [`Landcruiser`, `Assets`, `Bikes`];
+for (let i = 0; i < transporation.length; i++) {
+    console.log("i would like to own a " + transporation[i]);
+}
